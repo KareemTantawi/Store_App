15f9da85628f50a1131fae50ca54b46b8e05e991
Kareem Tantawi <kareem01229526319@gmail.com>
2024-12-22 17:14:33 +0200

    anv create done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:store_app/core/app/env.variable.dart';
 import 'package:store_app/store_app.dart';
 
-void main() {
-  runApp(
-    const StoreApp(),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    (value) {
+      runApp(
+        const StoreApp(),
+      );
+    },
   );
 }
